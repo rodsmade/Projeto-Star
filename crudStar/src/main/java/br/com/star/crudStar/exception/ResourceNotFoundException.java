3ee0bc4fda22a17c880b411ee12866987314d379
rdrggrgld <rodrigogregoldo@gmail.com>
2019-07-28 20:22:19 -0300

    CRUD basico finalizado

---
+++ crudStar/src/main/java/br/com/star/crudStar/exception/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package br.com.star.crudStar.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends Exception {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
--- crudStar/src/main/java/br/com/star/crudStar/repository/DadosPessoaisRepository.java
+++ crudStar/src/main/java/br/com/star/crudStar/repository/DadosPessoaisRepository.java
@@ -2,6 +2,18 @@ package br.com.star.crudStar.repository;
 
 import br.com.star.crudStar.model.DadosPessoais;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
+import java.math.BigDecimal;
 
 public interface DadosPessoaisRepository extends JpaRepository<DadosPessoais,Long> {
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE DadosPessoais d SET d.url_foto_perfil = :url WHERE d.id = :id")
+    void updateUrlFotoPerfilById(@Param("url") String urlFotoPerfil, @Param("id") Long id);
+
 }
